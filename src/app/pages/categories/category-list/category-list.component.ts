196eb8fa05a8770321a18c5a221b9b2ddc43b82a
Leandro Santos <insideincloud@gmail.com>
2019-08-26 15:53:19 -0300

    Implementando o CategoryList Component

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CategoryService } from '../shared/category.service';
+import { Category } from '../shared/category.model';
+
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
 export class CategoryListComponent implements OnInit {
+  categories: Category[] = [];
 
-  constructor() { }
+  constructor(
+              private categoryservice: CategoryService            
+              ) { }
 
   ngOnInit() {
+    this.categoryservice.getAll().subscribe(
+      categories => this.categories = categories,
+      error => alert('Erro ao carregar a lista')
+    )
   }
 
+  deleteCategory(category) {
+    const mustDelete = confirm('Deseja realmente excluir este item?')
+
+    if (mustDelete) {
+      this.categoryservice.delete(category.id).subscribe(
+        () => this.categories = this.categories.filter(element => element != category),
+        () => alert("Erro ao tentar Ecluir")
+      )
+    }
+  }
 }
